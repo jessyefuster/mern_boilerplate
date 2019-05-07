6359eb1070f953111dee84f9479c855618a26255
jessyefuster <jfuster@student.42.fr>
2019-05-07 22:25:31 +0200

    Working npm start script (nodemon + webpack)

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+const publicPath = path.resolve(__dirname, '..', 'public');
 
-app.get('/', (req, res) => {
-   res.send('Hello from Express!');
-});
+app.use(express.static(publicPath));
 
 app.listen(3000, () => {
    console.log('MERN Boilerplate listening on port 3000!');
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './client/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public'
+  }
+};
